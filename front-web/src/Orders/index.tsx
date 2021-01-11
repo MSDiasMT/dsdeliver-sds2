6c589a2c41502f46da3dcd89de63be9b92dea0a3
Moisés S. Dias <moisessdias@gmail.com>
2021-01-10 23:59:20 -0400

    Select Product

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -6,8 +6,11 @@ import { OrderLocationdata, Product } from './types';
 import { fetchProducts } from '../api';
 import { error } from 'console';
 import OrderLocation from './OrderLocation';
+import OrderSummary from './OrderSummary';
+import Footer from '../Footer';
 function Orders() {
     const [products, setProducts] = useState<Product[]>([]);
+    const [selectedProducts, setSelectedProducts] = useState<Product[]>([]);
     const [orderLocation, setOrderLocation] = useState<OrderLocationdata>();
 
     useEffect(() => {
@@ -15,12 +18,32 @@ function Orders() {
             .then(Response => setProducts(Response.data))
             .catch(error => console.log(error))
     }, []);
+
+    const handleSelectProduct = (product: Product) => {
+        const isAlreadySelected = selectedProducts.some(item => item.id === product.id);
+
+        if (isAlreadySelected) {
+            const selected = selectedProducts.filter(item => item.id !== product.id);
+            setSelectedProducts(selected);
+        } else {
+            setSelectedProducts(previous => [...previous, product]);
+        }
+    }
+
+
     return (
-        <div className="orders-container">
-            <StepsHeader />
-            <ProductsList products={products} />
-            <OrderLocation onChangeLocation={location => setOrderLocation(location)} />
-        </div>
+        <>
+            <div className="orders-container">
+                <StepsHeader />
+                <ProductsList
+                    products={products}
+                    onSelectProduct={handleSelectProduct}
+                />
+                <OrderLocation onChangeLocation={location => setOrderLocation(location)} />
+                <OrderSummary />
+            </div>
+            <Footer />
+        </>
     )
 }
 export default Orders;
